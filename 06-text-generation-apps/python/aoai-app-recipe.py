512bec87df1cceac8c58e30fab45479fd51f1cf0
Inês Soares <inesamil@hotmail.com>
2024-04-07 12:00:02 +0100

    Adapt course-06 and course-07 assignments

--- 06-text-generation-apps/python/aoai-app-recipe.py
+++ 06-text-generation-apps/python/aoai-app-recipe.py
@@ -1,18 +1,12 @@
-from openai import AzureOpenAI
 import os
-import dotenv
+from ollama import Client
+from dotenv import load_dotenv
+load_dotenv()
 
-# import dotenv
-dotenv.load_dotenv()
-
-# configure Azure OpenAI service client 
-client = AzureOpenAI(
-  azure_endpoint = os.environ["AZURE_OPENAI_ENDPOINT"], 
-  api_key=os.environ['AZURE_OPENAI_KEY'],  
-  api_version = "2023-10-01-preview"
-  )
-
-deployment=os.environ['AZURE_OPENAI_DEPLOYMENT']
+client = Client(
+    host = os.environ['OLLAMA_ENDPOINT']
+    )
+model = "mistral" # Change the model accordingly
 
 no_recipes = input("No of recipes (for example, 5: ")
 
@@ -23,22 +17,30 @@ filter = input("Filter (for example, vegetarian, vegan, or gluten-free: ")
 # interpolate the number of recipes into the prompt an ingredients
 prompt = f"Show me {no_recipes} recipes for a dish with the following ingredients: {ingredients}. Per recipe, list all the ingredients used, no {filter}: "
 messages = [{"role": "user", "content": prompt}]
+options = {
+  "temperature": 0.8,
+  "num_predict": 1200
+}
 
-completion = client.chat.completions.create(model=deployment, messages=messages, max_tokens=600, temperature = 0.1)
+completion = client.chat(model=model, messages=messages, options=options)
 
 
 # print response
 print("Recipes:")
-print(completion.choices[0].message.content)
+print(completion['message']['content'])
 
-old_prompt_result = completion.choices[0].message.content
+old_prompt_result = completion['message']['content']
 prompt_shopping = "Produce a shopping list, and please don't include ingredients that I already have at home: "
 
 new_prompt = f"Given ingredients at home {ingredients} and these generated recipes: {old_prompt_result}, {prompt_shopping}"
 messages = [{"role": "user", "content": new_prompt}]
-completion = client.chat.completions.create(model=deployment, messages=messages, max_tokens=600, temperature=0)
+options = {
+  "temperature": 0,
+  "num_predict": 50
+}
+completion = client.chat(model=model, messages=messages, options=options)
 
 # print response
 print("\n=====Shopping list ======= \n")
-print(completion.choices[0].message.content)
+print(completion['message']['content'])
 
